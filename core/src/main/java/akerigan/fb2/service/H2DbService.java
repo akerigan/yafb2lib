5a27e93b283b2a9937910020cec873a11ea867ba
Vlad Vinichenko <akerigan@gmail.com>
2010-05-01 13:56:09 +0400

    db service updated

--- core/src/main/java/akerigan/fb2/service/H2DbService.java
+++ core/src/main/java/akerigan/fb2/service/H2DbService.java
@@ -17,10 +17,16 @@ public class H2DbService extends DbService {
     public void init() {
         Set<String> tables = new TreeSet<String>(
                 template.query("select table_name from information_schema.tables", StringMapper.getInstance()));
-
-        if (!tables.contains("TEST")) {
-            template.update("create table test(id identity primary key, name varchar(255))");
+        if (!tables.contains("books")) {
+            template.update("create table books(book_id identity primary key, sha1 char(40)," +
+                    " container varchar(256), file varchar(256), bigint size)");
+            template.update("create index books_sha1_idx ON books(sha1)");
+        }
+        if (!tables.contains("books_description")) {
+            template.update("create table books_description(int book_id, name varchar(256), value varchar(4096))");
+            template.update("create index books_id_idx ON books_description(book_id)");
         }
     }
+
 
 }
